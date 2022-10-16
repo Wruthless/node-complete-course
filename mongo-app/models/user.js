a7f9705459f5c625946eed0f67de106ee48accec
Wes Gonzales <wesgonzales@Wess-MacBook-Air.local>
2022-10-16 09:04:04 -0400

    Multiple items and quanity functionality.

--- mongo-app/models/user.js
+++ mongo-app/models/user.js
@@ -20,16 +20,41 @@ class User {
     // 1:1 relationship between users and carts. This is opposed to having a
     // separate Cart model (cart.js).
     addToCart(product) {
-        // const cartProduct = this.cart.items.findIndex(cp => {
-        //     return cp._id === product._id;
-        // });
+        // Check if current item exists to avoid overwriting cart contents.
+        const cartProductIndex = this.cart.items.findIndex(cp => {
+            // The id from mongo is treated as a string in JS -- however, it is
+            // not a string. Therefore, you need to convert the product id coming
+            // from the database to a string and compare it to a string. If
+            // you are using the triple equality operator which test not only for
+            // value, but also type.
+            return cp.productId.toString() === product._id.toString();
+        });
+
+        let newQuantity = 1;
+
+        // A new array with all items in the cart
+        const updatedCartItems = [...this.cart.items];
+
+        if (cartProductIndex >= 0) {
+            // If the item exist, update quantity
+            newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+            updatedCartItems[cartProductIndex].quantity = newQuantity;
+        } else {
+            // If the item does not exist, add the item tot he cart
+            updatedCartItems.push({
+                productId: new ObjectId(product._id),
+                quantity: newQuantity
+            });
+        }
         /*
             The following will store all product data, which we do not want.
             Notice the spread syntax is being used on product -- ...product
 
             const updatedCart = { items: [{ ...product, quantity: 1 }] }; 
         */
-        const updatedCart = { items: [{ productId: new ObjectId(product._id), quantity: 1 }] };
+        const updatedCart = {
+            items: updatedCartItems
+        };
         const db = getDb();
 
         return db.collection('users').updateOne(
